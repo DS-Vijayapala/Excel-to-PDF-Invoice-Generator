3a944d224d65b61bd25deb24a4f9fac52ff72b6c
Dineth <dinethfiverr23@gmail.com>
2025-02-07 12:41:28 +0530

    Day 25 Done

--- main.py
+++ main.py
@@ -10,15 +10,18 @@ import pathlib
 filepaths = glob.glob("Invoices/*.xlsx")
 
 for filepath in filepaths:
+
+    # Read the excel file using pandas and create a pdf object using FPDF and add a page to it
     df = pd.read_excel(filepath , sheet_name='Sheet 1')
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
 
-    filename = pathlib.Path(filepath).stem
+    # Get the filename without the extension and split it by "-" to get the invoice number 
+    filename = pathlib.Path(filepath).stem 
     invoice_nr = filename.split("-")[0]
 
-
-    pdf.set_font("Arial", size = 18, style='B')
+    # Add the invoice number to the pdf
+    pdf.set_font(family="Times", size = 18, style='B')
     pdf.cell(w=50, h=8, txt = f"Invoice {invoice_nr}", ln = True, align = 'L')
 
     pdf.output(f"PDFs/{filename}.pdf")
